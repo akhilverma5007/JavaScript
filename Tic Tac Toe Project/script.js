a40cdeb2013d514046394777a53eb682308e95b1
Akhil Verma <akhilverma@Akhils-MacBook-Pro.local>
2024-02-14 20:47:48 +0530

    Tic Tac Toe

---
+++ Tic Tac Toe Project/script.js
@@ -0,0 +1,62 @@
+
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#resetBtn");
+let newGameBtn = document.querySelector("#new-Btn");
+let msgContainer = document.querySelector("#msgContainer");
+let msg = document.querySelector("#msg");
+
+let turnO = true;//playerX, playerO
+
+//Use of 2D-Array
+const winPatterns = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("Box was clicked");
+        if(turnO){
+            box.innerText = "O";
+            turnO = false;
+        }else{
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
+        checkWinner();
+    });
+});
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+}
+
+const checkWinner = () => {
+    for(pattern of winPatterns){
+        // console.log(pattern[0],
+        //             pattern[1],
+        //             pattern[2]);
+        // console.log(boxes[pattern[0]].innerText,
+        //             boxes[pattern[1]].innerText,
+        //             boxes[pattern[2]].innerText
+        // );
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
+        if(pos1Val === pos2Val & pos2Val === pos3Val){
+            console.log("Winner",pos1Val);
+            showWinner(pos1Val);
+        }
+    }
+    }
+};
